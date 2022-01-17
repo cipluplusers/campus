0d0828b7bb97bc13996ccc1c7fc1397f775b95b5
Anna <annagavrilenko778@gmail.com>
2022-01-18 00:46:44 +0200

    изменение cpp

---
+++ ClassMark.h
@@ -0,0 +1,16 @@
+#ifndef CLASSMARK_H
+#define CLASSMARK_H
+
+using namespace std;
+
+
+class Mark {
+public:
+
+	unsigned long id;
+	string subjectId;
+	string teacherId;
+	int value;
+};
+
+#endif
--- classMark.cpp
+++ classMark.cpp
@@ -1,17 +1,1 @@
-﻿#include <iostream>
-#include <string>
-using namespace std;
-
-class Mark {
-public:
-	int Id;
-	string SubjectId;
-	string TeacherId;
-	int Value;
-};
-
-int main()
-{
-	
-}
-
+﻿#include "ClassMark.h"
