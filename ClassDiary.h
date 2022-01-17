c87c7a72dc01b41efb1f40e754479af12f2c9f96
Vladislava <vladislavayakhnenko@gmail.com>
2022-01-18 00:44:09 +0200

    Changed ClassDiary.cpp

---
+++ ClassDiary.h
@@ -0,0 +1,13 @@
+#ifndef CLASSDIARY_H
+#define CLASSDIARY_H
+
+using namespace std;
+
+class Diary {
+public:
+    unsigned long id;
+    string studentId;
+    int marks;
+};
+
+#endif /* CLASSDIARY_H */
