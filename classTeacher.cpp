8d7697d276d04343367c932f3db5d89eaabc88ad
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-01-17 21:42:54 +0200

    Add ClassTeaher main file

---
+++ classTeacher.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "classTeacher.h"
+
+using namespace std;
+
+int main()
+{
+	Teacher Minar;
+	Minar.name = "Pidoras";
+	Minar.surname = "Ebaniy";
+	cout << "Minar " << Minar.name << " " << Minar.surname << endl;
+}
