1ecb2a7599289112e1b9b6594074c18bfff5794d
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-01-17 22:18:17 +0200

    changed .cpp file

--- classTeacher.cpp
+++ classTeacher.cpp
@@ -1,13 +1,3 @@
 #include <iostream>
 #include <string>
 #include "classTeacher.h"
-
-using namespace std;
-
-int main()
-{
-	Teacher Minar;
-	Minar.name = "Oleksandr";
-	Minar.surname = "Minarchenko";
-	cout << "Teacher - " << Minar.name << " " << Minar.surname << endl;
-}
