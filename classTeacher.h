0ae758bf3e9bb3ce588af239203b72243994d3b4
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-01-17 21:33:44 +0200

    Add ClassTeacher header file

---
+++ classTeacher.h
@@ -0,0 +1,20 @@
+#ifndef ClassTeacher
+#define ClassTeacher
+
+using namespace std;
+
+class Teacher
+{
+public:
+	string name;
+	string surname;
+	string secondName;
+	string dateOfBirght;
+	int id;
+	int departmentId;
+	int subjectId;
+
+};
+
+#endif // ClassTeacher
+
