3bd89b13339191e943a0a20e37f1a2498d5facc5
Anna <annagavrilenko778@gmail.com>
2022-01-17 21:29:37 +0200

    changed classMark

---
+++ classMark.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Mark {
+public:
+	typedef string yeba;
+	int Id;
+	string SubjectId;
+	yeba TeacherId;
+	int Value;
+};
+
+int main()
+{
+	
+}
+
