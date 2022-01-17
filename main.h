5b263698ca9885b0f709a282e474dcac51ff3fae
vkulhova <vladyslava.kulhova@gmail.com>
2022-01-18 00:30:36 +0200

    Added header+added file .h

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef CLASSLEARNINGPROGRAM_H
+#define CLASSLEARNINGPROGRAM_H
+
+using namespace std;
+
+class LearningProgram
+{
+public:
+    
+    unsigned long ID;
+    string Subject;
+};
+
+int main()
+{
+    
+}
+
+#endif
