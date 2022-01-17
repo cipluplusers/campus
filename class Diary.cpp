dc9c1f63553e3c14651c3c702790a8284661c87f
Vladislava <vladislavayakhnenko@gmail.com>
2022-01-17 21:06:22 +0200

    Added classDiary.cpp

---
+++ class Diary.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Diary {
+public:
+    string Id;
+    string StudentId;
+    string Marks;
+};
+
+int main()
+{
+    system("pause > 0");
+    return 0;
+}
+
---
+++ classDiary.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Diary {
+public:
+    string Id;
+    string StudentId;
+    string Marks;
+};
+
+int main()
+{
+    system("pause > 0");
+    return 0;
+}
