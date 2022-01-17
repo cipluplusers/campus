1c9af16159aa7f5097dd9464bdb3f607d1d30a59
vkulhova <vladyslava.kulhova@gmail.com>
2022-01-18 00:43:01 +0200

    changed name of file

---
+++ ClassLP.h
@@ -0,0 +1,14 @@
+#ifndef CLASSLEARNINGPROGRAM_H
+#define CLASSLEARNINGPROGRAM_H
+
+using namespace std;
+
+class LearningProgram
+{
+public:
+    
+    unsigned long ID;
+    string Subject;
+};
+
+#endif
