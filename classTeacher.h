20a2086f3c2c2d6551e78651e769975452cdc497
DmitryiSamborskiy <64884428+SamborskiyD@users.noreply.github.com>
2022-01-17 22:50:02 +0200

    Dima (#2)
    * test branch
    
    * add test.cpp
    
    * changed test.cpp
    
    * Add ClassTeacher header file
    
    * Add ClassTeaher main file
    
    * Changed ClassTeacher main file and header file
    
    * Changed ClassTeacher header file
    
    * changed .cpp file

---
+++ classTeacher.h
@@ -0,0 +1,20 @@
+#ifndef ClassTeacher
+#define ClassTeacher
+
+using namespace std;
+
+class Teacher
+{
+public:
+	string name;
+	string surname;
+	string secondName;
+	string dateOfBirght;
+	unsigned long id;
+	unsigned long departmentId;
+	unsigned long subjectId;
+
+};
+
+#endif // ClassTeacher
+
