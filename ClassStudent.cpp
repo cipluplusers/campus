9961dedb881bc280a3664ba00a623fcfe69670c9
Olya <darwinwoda@gmail.com>
2022-01-17 15:00:39 +0200

    Add Class Student

---
+++ ClassStudent.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Student
+{
+public:
+    
+    string name;
+    string surname;
+    string second_name;
+    string dateOfBirth;
+    string id;
+    string department_id;
+    string diary_id;
+    string learningProgramming_id;
+    string courseStartDate;
+    
+};
+
+
+int main()
+{
+
+}
