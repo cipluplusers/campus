b2d26f6108f2b50526ca2e3e296a6bf109612784
vkulhova <vladyslava.kulhova@gmail.com>
2022-01-17 20:41:54 +0200

    new branch vlkulh

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  branch_test
+//
+//  Created by Владислава Кульгова on 17.01.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
