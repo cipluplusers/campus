cbef76b1e8ab01c61a3d400cfb14fa2c1930cedb
vkulhova <vladyslava.kulhova@gmail.com>
2022-01-18 17:16:08 +0200

    deleted bodies of methods of class

--- LinkedList.h
+++ LinkedList.h
@@ -24,73 +24,14 @@ public:
 		head = nullptr;
 	}
 
-	void addToPosition(int value, int position)
-	{
-		Node* node = new Node;
-		node->data = value;
-		if (head == nullptr)
-		{
-			node->next = node;
-			node->prev = node;
-			head = node;
-		}
-		else
-		{
-			Node* p = head;
-			for (int i = position; i > 1; i--)
-			{
-				p = p->next;
-			}
-			p->prev->next = node;
-			node->prev = p->prev;
-			node->next = p;
-			p->prev = node;
-		}
-	}
+    void addToPosition(int value, int position);
 
 
-	int deleteFromPosition(int position)
-	{
-		if (head == nullptr)
-		{
-			cout << "\nList is empty\n\n";
-			return 0;
-		}
+    int deleteFromPosition(int position);
+    
 
-		if (head == head->next)
-		{
-			delete head;
-			head = nullptr;
-		}
-		else
-		{
-			Node* a = head;
-			for (int i = position; i > 1; i--) a = a->next;
-			if (a == head) head = a->next;
-			a->prev->next = a->next;
-			a->next->prev = a->prev;
-			delete a;
-		}
-	}
-
-
-	void printList()
-	{
-		if (head == nullptr) cout << "\nList is empty\n\n";
-		else
-		{
-			Node* current = head;
-			cout << "\nList: ";
-			do
-			{
-				cout << current->data << " ";
-				current = current->next;
-			} while (current != head);
-			cout << "\n\n";
-		}
-	}
-
-
+    void printList();
+    
 };
 
 
