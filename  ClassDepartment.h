0764384108026d34633a757ff37cf673e201b195
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-01-18 12:42:52 +0200

    rename flles

---
+++  ClassDepartment.h
@@ -0,0 +1,15 @@
+#ifndef CLASSDEPARTMENT_H
+#define CLASSDEPARTMENT_H
+
+using namespace std;
+
+class Department
+{
+public:
+
+	string caption;
+	unsigned long id;
+};
+
+#endif // CLASSDEPARTMENT_H
+
