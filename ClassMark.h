7e3bd86d2f6b0d55826540aa6ca48a654a6488f6
AnnaHavrilenko <97827615+AnnaHavrilenko@users.noreply.github.com>
2022-01-18 09:36:02 +0200

    Anna (#3)
    * changed classMark
    
    * с новым годом
    
    * изменение cpp
    
    * .
    
    * .

---
+++ ClassMark.h
@@ -0,0 +1,18 @@
+#ifndef CLASSMARK_H
+#define CLASSMARK_H
+
+
+using namespace std;
+
+
+class Mark {
+public:
+
+	unsigned long id;
+	string subjectId;
+	string teacherId;
+	int value;
+};
+
+
+#endif
