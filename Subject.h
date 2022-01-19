8a71e41d19cec75d4dfec4f421df584e3d65b069
AndriyBelchik <97786015+akumake@users.noreply.github.com>
2022-01-19 13:44:56 +0200

    Add files via upload

---
+++ Subject.h
@@ -0,0 +1,12 @@
+#ifndef SUBJECTS_H
+#define SUBJECTS_H
+using namespace std;
+class Subject
+{
+public:
+	string SubjectCaption;
+	unsigned long SubjectId;
+	unsigned long  SubjectCountOfCredits;
+};
+
+#endif
