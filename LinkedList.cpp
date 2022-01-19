994728b7d9d9afeffbdfda9395144c53982ca7ea
Anna <annagavrilenko778@gmail.com>
2022-01-19 17:21:36 +0200

    -треш

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,73 +1,69 @@
-<<<<<<< HEAD
-#include "LinkedList.h"
-=======
 #include "LinkedList.h"
 
 template <typename T>
 
 void Node::addToPosition(T value, int position)
 {
-    Node* node = new Node;
-    node->data = value;
-    if (head == nullptr)
-    {
-        node->next = node;
-        node->prev = node;
-        head = node;
-    }
-    else
-    {
-        Node* p = head;
-        for (int i = position; i > 1; i--)
-        {
-            p = p->next;
-        }
-        p->prev->next = node;
-        node->prev = p->prev;
-        node->next = p;
-        p->prev = node;
-    }
+	Node* node = new Node;
+	node->data = value;
+	if (head == nullptr)
+	{
+		node->next = node;
+		node->prev = node;
+		head = node;
+	}
+	else
+	{
+		Node* p = head;
+		for (int i = position; i > 1; i--)
+		{
+			p = p->next;
+		}
+		p->prev->next = node;
+		node->prev = p->prev;
+		node->next = p;
+		p->prev = node;
+	}
 }
 
 
 int Node::deleteFromPosition(int position)
 {
-    if (head == nullptr)
-    {
-        cout << "\nList is empty\n\n";
-        return 0;
-    }
+	if (head == nullptr)
+	{
+		cout << "\nList is empty\n\n";
+		return 0;
+	}
 
-    if (head == head->next)
-    {
-        delete head;
-        head = nullptr;
-    }
-    else
-    {
-        Node* a = head;
-        for (int i = position; i > 1; i--) a = a->next;
-        if (a == head) head = a->next;
-        a->prev->next = a->next;
-        a->next->prev = a->prev;
-        delete a;
-    }
+	if (head == head->next)
+	{
+		delete head;
+		head = nullptr;
+	}
+	else
+	{
+		Node* a = head;
+		for (int i = position; i > 1; i--) a = a->next;
+		if (a == head) head = a->next;
+		a->prev->next = a->next;
+		a->next->prev = a->prev;
+		delete a;
+	}
 }
 
 
 void Node::printList()
 {
-    if (head == nullptr) cout << "\nList is empty\n\n";
-    else
-    {
-        Node* current = head;
-        cout << "\nList: ";
-        do
-        {
-            cout << current->data << " ";
-            current = current->next;
-        } while (current != head);
-        cout << "\n\n";
-    }
+	if (head == nullptr) cout << "\nList is empty\n\n";
+	else
+	{
+		Node* current = head;
+		cout << "\nList: ";
+		do
+		{
+			cout << current->data << " ";
+			current = current->next;
+		} while (current != head);
+		cout << "\n\n";
+	}
 }
->>>>>>> 72daaa1ee166d539f19067566ed7d188b0e2820c
--- LinkedList.h
+++ LinkedList.h
@@ -3,27 +3,17 @@
 
 using namespace std;
 
-<<<<<<< HEAD
-
-struct Node
-{
-	int data;
-=======
 template <typename T>
 
 struct Node
 {
 	T data;
->>>>>>> 72daaa1ee166d539f19067566ed7d188b0e2820c
 	Node* next;
 	Node* prev;
 };
 
-<<<<<<< HEAD
-=======
 template <class T>
 
->>>>>>> 72daaa1ee166d539f19067566ed7d188b0e2820c
 class LinkedList
 {
 public:
@@ -37,89 +27,15 @@ public:
 		head = nullptr;
 	}
 
-<<<<<<< HEAD
-	void addToPosition(int value, int position)
-	{
-		Node* node = new Node;
-		node->data = value;
-		if (head == nullptr)
-		{
-			node->next = node;
-			node->prev = node;
-			head = node;
-		}
-		else
-		{
-			Node* p = head;
-			for (int i = position; i > 1; i--)
-			{
-				p = p->next;
-			}
-			p->prev->next = node;
-			node->prev = p->prev;
-			node->next = p;
-			p->prev = node;
-		}
-	}
+	void addToPosition(T value, int position);
 
 
-	int deleteFromPosition(int position)
-	{
-		if (head == nullptr)
-		{
-			cout << "\nList is empty\n\n";
-			return 0;
-		}
-
-		if (head == head->next)
-		{
-			delete head;
-			head = nullptr;
-		}
-		else
-		{
-			Node* a = head;
-			for (int i = position; i > 1; i--) a = a->next;
-			if (a == head) head = a->next;
-			a->prev->next = a->next;
-			a->next->prev = a->prev;
-			delete a;
-		}
-	}
+	int deleteFromPosition(int position);
 
 
-	void printList()
-	{
-		if (head == nullptr) cout << "\nList is empty\n\n";
-		else
-		{
-			Node* current = head;
-			cout << "\nList: ";
-			do
-			{
-				cout << current->data << " ";
-				current = current->next;
-			} while (current != head);
-			cout << "\n\n";
-		}
-	}
-
+	void printList();
 
 };
 
 
 #endif
-=======
-    void addToPosition(T value, int position);
-
-
-    int deleteFromPosition(int position);
-    
-
-    void printList();
-    
-};
-
-
-#endif
->>>>>>> 72daaa1ee166d539f19067566ed7d188b0e2820c
