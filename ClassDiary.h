86d01461ff43a466193aad3e2f96e592b1026d4a
Olya <darwinwoda@gmail.com>
2022-01-20 20:45:58 +0200

    modified:   ClassDiary.h

--- ClassDiary.h
+++ ClassDiary.h
@@ -2,10 +2,7 @@
 #define CLASSDIARY_H
 #include <string>
 #include "LinkedList.h"
-<<<<<<< HEAD
 #include "ClassMark.h"
-=======
->>>>>>> df188ce150d40f1d5f49bfa5958e80b27dc803fb
 
 using namespace std;
 
@@ -15,12 +12,7 @@ public:
     
     unsigned long id;
     unsigned long studentId;
-<<<<<<< HEAD
     LinkedList<Mark> Marks;
-=======
-    LinkedList<Marks> Marks;
->>>>>>> df188ce150d40f1d5f49bfa5958e80b27dc803fb
-
     string toString();
 
 	friend ostream& operator<< (ostream& out, Diary& diary);
