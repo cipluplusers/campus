35582cc43251ec303a26d0d28f03a537ea76a839
Vladislava <vladislavayakhnenko@gmail.com>
2022-01-26 15:48:32 +0200

    .

--- ClassDataBase.h
+++ ClassDataBase.h
@@ -132,6 +132,13 @@ public:
 	Student getStudentByName(string name);
 	Student getStudentById(unsigned long id);
 	Student getStudentBySurname(string surname);
+
+	LearningProgram getLearningProgramById(unsigned long id);
+	LearningProgram getLearningProgramByTitle(string title);
+
+	Department getDepartmentByTitle(string title);
+	Department getDepartmentById(unsigned long id);
+
 };
 
 
