55bc72e26a0b64b3d94c3c195b7e707025721aaf
VladislavaYakhnenko <97827651+VladislavaYakhnenko@users.noreply.github.com>
2022-01-27 11:05:28 +0200

    Vlada ya (#35)
    * Added classDiary.cpp
    
    * Delete classDiary.cpp
    
    * Changed ClassDiary.cpp
    
    * .
    
    * Delete
    
    * .
    
    * .
    
    * .
    
    * .
    
    * Added getString
    
    * Added GetString
    
    * Changed LinkedList.cpp
    
    * Changed LinkedList.h
    
    * .
    
    * .
    
    * .
    
    * .
    
    * Changed LinkedList<Subject>
    
    * Changed LinkedList<Subject>
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    Co-authored-by: Olya <97823692+WinterSakuraa@users.noreply.github.com>
    Co-authored-by: Dima Selyutin <64527843+diselyuti@users.noreply.github.com>

--- ClassDataBase.h
+++ ClassDataBase.h
@@ -132,6 +132,18 @@ public:
 	Student getStudentById(unsigned long id);
 	Student getStudentBySurname(string surname);
 
+
+	Student getStudentByName(string name);
+	Student getStudentById(unsigned long id);
+	Student getStudentBySurname(string surname);
+
+
+	LearningProgram getLearningProgramById(unsigned long id);
+	LearningProgram getLearningProgramByTitle(string title);
+
+
+	Department getDepartmentByTitle(string title);
+	Department getDepartmentById(unsigned long id);
 };
 
 
--- ClassSubject.h
+++ ClassSubject.h
@@ -15,29 +15,6 @@ public:
 	unsigned long  subjectCountOfCredits;
 
 	string toString();
-    string serialize();
-
-	friend ostream& operator<< (ostream& out, Subject& subject);
-};
-#endif
-=======
-#ifndef SUBJECTS_H
-#define SUBJECTS_H
-#include <string>
-
-
-using namespace std;
-
-
-class Subject
-{
-public:
-    
-	string subjectCaption;
-	unsigned long subjectId;
-	unsigned long  subjectCountOfCredits;
-
-	string toString();
   string serialize();
 
 	friend ostream& operator<< (ostream& out, Subject& subject);
