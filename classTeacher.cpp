025fb606f4b2fcd4d04333cc7d9a9f54a95fc477
vkulhova <vladyslava.kulhova@gmail.com>
2022-01-30 18:30:02 +0200

    определила фукнцию десериализации

--- classTeacher.cpp
+++ classTeacher.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+
+#include <cstdlib>
 #include "ClassTeacher.h"
 #include "ClassDate.h"
 
@@ -28,6 +30,36 @@ string Teacher::serialize()
             "|ID|" + to_string( id ) +
             "|DepartmentId|" + to_string( departmentId ) +
             "|SubjectId|" + to_string( subjectId ) + "|";
+}
+
+
+Teacher Teacher::deserialize(string str)
+{
+    string arr[14];
+    string arr1[3];
+    for (int i = 0; i < 14; i++)
+    {
+        getline(cin, str, '|');
+        arr[i] = str;
+    }
+    
+    for (int j = 0; j < 3; j++)
+    {
+        getline(cin, arr[7], '.');
+        arr1[j] = arr[7];
+    }
+    
+    int day = stoi(arr1[0]);
+    int month = stoi(arr1[1]);
+    int year = stoi(arr1[2]);
+    
+    unsigned long id = stoul(arr[9], nullptr, 10);
+    unsigned long departmentId = stoul(arr[11], nullptr, 10);
+    unsigned long subjectId = stoul(arr[13], nullptr, 10);
+    
+    Teacher object = {arr[1], arr[3], arr[5], {day, month, year}, id, departmentId, subjectId};
+    
+    return object;
 }
 
 
